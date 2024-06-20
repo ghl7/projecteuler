c2909a4d40653eb1dc33795b30436a91a56ceef8
ghl7 <39172684+ghl7@users.noreply.github.com>
2024-06-20 15:46:30 -0500

    update

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from problems_11_20 import process_problems_11_20
 from problems_1_10 import process_problems_1_10
 
+def sum_of_divisors(n):
+    z = 0
+    for i in range(1, n):
+        if n % i == 0:
+            z = z + i
+    return z
+
 def main():
     # process_problems_1_10()
     # process_problems_11_20()
 
-    N = 220
-    for x in range(1, N + 1):
-        if (N % x == 0):
-            print(x)
-
-    # process_problem(problem_11, 4)
-    # process_problem(problem_12, 500)
-    # process_problem(problem_13, 10)
-    # process_problem(problem_14, 1000000)
-    # process_problem(problem_15, 20)
-    # process_problem(problem_16, 1000)
-    # process_problem(problem_17, 1000)
-    # process_problem(problem_18, "")
-    # process_problem(problem_19,"")
-    # process_problem(problem_20, 100)
+    print(sum_of_divisors(220))
+    print(sum_of_divisors(284))
+    if (sum_of_divisors(220) == 284) and (sum_of_divisors(284) == 220):
+        print('220, 284 are amicable numbers')
 
 
 if __name__ == '__main__':
