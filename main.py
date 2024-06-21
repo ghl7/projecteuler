a0c5d5b9c8f20560358802398fd84138ec3ffc34
ghl7 <39172684+ghl7@users.noreply.github.com>
2024-06-21 14:06:25 -0500

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from problems_11_20 import process_problems_11_20
 from problems_1_10 import process_problems_1_10
+from util import process_problem
 
 def sum_of_divisors(n):
     z = 0
@@ -8,9 +9,24 @@ def sum_of_divisors(n):
             z = z + i
     return z
 
+def problem_21(args):
+    total_sum = 0
+    a = 100
+    b = int(1.2 * a)
+    for i in range(a+2, b, 2):
+        if (a > args[1]) or (b > args[1]):
+            return total_sum
+        if (sum_of_divisors(a) == i) and (sum_of_divisors(a) == i):
+            print(a)
+            print(i)
+            total_sum = total_sum + i + a
+
+
 def main():
     # process_problems_1_10()
     # process_problems_11_20()
+
+    process_problem(problem_21, 10000)
 
     print(sum_of_divisors(220))
     print(sum_of_divisors(284))
