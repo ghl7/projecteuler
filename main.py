3edf6200135a15f7becd09a72bb3fec825d11c65
ghl7 <39172684+ghl7@users.noreply.github.com>
2024-06-25 08:58:59 -0500

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from problems_11_20 import process_problems_11_20
 from problems_1_10 import process_problems_1_10
 from util import process_problem
+import re
 
 
 def sum_of_divisors(n):
@@ -17,32 +18,39 @@ def problem_21(args):
     a = 200
 
     while True:
-        a = a + 2
+        a = a + 1
         done = False
         loc_sum = sum_of_divisors(a)
-        for i in range(a, args[1], 2):
+        for i in range(a, args[1], 1):
             if done:
                 break
-            if a >= (args[1] - 2):
+            if a >= (args[1] - 1):
                 return total_sum
             if (loc_sum == i) and (sum_of_divisors(i) == a) and (a != i):
-                # txt = f"({a},{i})"
-                # print(txt)
+                # print(f"({a},{i})")
                 total_sum = total_sum + i + a
                 a = i
                 done = True
 
+def problem_22(args):
+    f = open(args[1])
+    t = f.read()
+    f.close()
+    print(t)
+    t = re.sub('"', '', t)
+    print(t)
+    x = re.split(',', t)
+    print(x)
+    x.sort()
+    print(x)
+    return 0
 
 def main():
     # process_problems_1_10()
     # process_problems_11_20()
 
-    process_problem(problem_21, 10000)
-
-    # print(sum_of_divisors(220))
-    # print(sum_of_divisors(284))
-    # if (sum_of_divisors(220) == 284) and (sum_of_divisors(284) == 220):
-    #     print('220, 284 are amicable numbers')
+    # process_problem(problem_21, 10000)
+    process_problem(problem_22, "0022_names.txt")
 
 
 if __name__ == '__main__':
